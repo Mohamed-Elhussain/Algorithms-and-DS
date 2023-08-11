f44296bffa1598d2e2b9f93ef3ad1694324e1e59
Mohamed-Elhussain <93033236+Mohamed-Elhussain@users.noreply.github.com>
2023-08-11 04:11:57 +0300

    Create main.cpp

---
+++ B Problems/Problem 16/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using ll = long long;
+using namespace std;
+
+int main(){
+
+    int t,n,k, cnt = 0;
+    cin>>t;
+    while(t--){
+        cin>>n>>k;
+        cnt = 0;
+        for(int i = 1; i <= n; ++i){
+            int first = i, second = n;
+            bool check = true;
+            for(int j = 0; j < k - 2; ++j){
+                int fx = first;
+                first = second - fx;
+                second = fx;
+                if(first > second || min(first, second) < 0){
+                    check = false; break;
+                }
+
+            }
+            if(check){
+                ++cnt;
+            }
+        }
+        cout<<cnt<<"\n";
+
+    }
+
+}
